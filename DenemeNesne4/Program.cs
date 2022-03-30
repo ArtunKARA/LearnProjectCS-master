a438d14edbc9c72c9ab976b9a62e55e092f12093
ArtunKARA <artunankara@gmail.com>
2022-03-30 14:46:34 +0300

    Update

--- DenemeNesne4/Program.cs
+++ DenemeNesne4/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace DenemeNesne4
 {
+    static class Hesap
+    {
+        static public void Alan(int r)
+        {
+            Console.WriteLine("Daire:"+Math.PI*r*r);
+        }
+        static public void Alan(int a,int b)
+        {
+            Console.WriteLine("Dörtgen:"+a*b);
+        }
+        static public void Çevre(int r)
+        {
+            Console.WriteLine("Daire:"+Math.PI*r*2);
+        }
+        static public void Çevre(int a,int b)
+        {
+            Console.WriteLine("Dörtgen:"+(a+b)*2);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Hesap.Alan(4);
+            Hesap.Alan(2, 3);
+            Hesap.Çevre(3, 4);
+            Hesap.Çevre(6, 3);
+
         }
     }
 }
