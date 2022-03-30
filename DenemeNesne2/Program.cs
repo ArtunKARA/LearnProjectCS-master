37cd1042cbebea19761d752a9a513d5e44e31542
ArtunKARA <artunankara@gmail.com>
2022-03-30 14:38:19 +0300

    UpdateProje dosyası ekle.

---
+++ DenemeNesne2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenemeNesne2
+{
+    abstract class Hesap
+    {
+        public int r;
+        public Hesap(int r)
+        {
+            this.r = r;
+        }
+        virtual public void Alan()
+        {
+
+        }
+        virtual public void Çevre()
+        {
+
+        }
+    }
+    sealed class Daire:Hesap
+    {
+        public Daire(int r):base(r)
+        {
+        }
+        public override void Alan()
+        {
+            Console.WriteLine("Alan"+Math.PI*Math.Pow(Convert.ToDouble(r),2));
+        }
+        public override void Çevre()
+        {
+            Console.WriteLine("Çevre:"+2*Math.PI*r);
+        }
+    }
+    sealed class Kare:Hesap
+    {
+        public Kare(int r) : base(r)
+        {
+
+        }
+        public override void Alan()
+        {
+            Console.WriteLine("Alan" +r*r );
+        }
+        public override void Çevre()
+        {
+            Console.WriteLine("Çevre:"+r*4);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Daire d1 = new Daire(4);
+            d1.Çevre();
+            d1.Alan();
+            Kare k1 = new Kare(6);
+            k1.Alan();
+            k1.Çevre();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DenemeNesne3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenemeNesne3
+{
+    class Hesap
+    {
+        public int r;
+        private int seç;
+        public Hesap(int r, int seç)
+        {
+            this.r = r;
+            this.seç = seç;
+        }
+        public void Çevre()
+        {
+            switch (seç)
+            {
+                case 1:
+                    Console.WriteLine("Çevre:" + r * 4);
+                    break;
+                case 2:
+                    Console.WriteLine("Çevre:" + Math.PI * r * 2);
+                    break;
+                default:
+                    Console.WriteLine("hatalı seçim");
+                    break;
+            }
+        }
+        
+        public void Alan()
+        {
+            switch (seç)
+            {
+                case 1:
+                    Console.WriteLine("Alan:" + r * 4);
+                    break;
+                case 2:
+                    Console.WriteLine("Alan:" + Math.PI * r * r);
+                    break;
+                default:
+                    Console.WriteLine("hatalı seçim");
+                    break;
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
